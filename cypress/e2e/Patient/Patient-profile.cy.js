92e5f7fcce5cb8f4211d92a4132bd00add09cc29
Saeed Ullah <saeedullah321@gmail.com>
2023-10-09 01:13:16 +0500

    Verify Patient Acuity score and flags in the patient profile header.

--- cypress/e2e/Patient/Patient-profile.cy.js
+++ cypress/e2e/Patient/Patient-profile.cy.js
@@ -1,23 +1,38 @@
 /// <reference types="cypress-xpath" />
 import { Login } from "../Login";
 import { PatientDrawer } from "../PageObject/PatientDrawer.js";
-import { faker } from '@faker-js/faker';
 
 let patientdrawer = new PatientDrawer;
 
 describe('Patient Profile Test ', () => {
-    it('Verify Patient Flags Header in the profile',()=>{
+    it('Verify Flags Header and Acuity Score in the  Patient Channel and in the Patient profile',()=>{
        
         
-       Login();
-        // open any top patient channel 
-        patientdrawer.OpenPatientChannel('Patient Profile');
+       Login();  // Login into the Hucu Webapp.
+        
+        patientdrawer.OpenPatientChannel('Allen Osinski');  // open any top patient channel 
         cy.wait(2000);
-        patientdrawer.ClickPatientDrawer();
-        patientdrawer.ChannelMenu('Patient Profile');
-        cy.get('div.patient-header-flags').should('have.length',2);
-        // cy.xpath("//div[@class='ui grid']//div[4]").find('.patient-header-flag');
-        // div[class='ui grid'] div:nth-child(4)
-  
+        let score;          
+        let patientFlags;
+        
+        cy.get('.score-text').invoke('text').then(text=>{
+            score=text;               // Get the Patient Acuity Score in the Patient Channel Header.
+        })
+
+        cy.get('span.patient-header-flag').then(el=> patientFlags=el.length); // Get the Patient Flags in the Patient Channel Header. 
+
+        patientdrawer.ClickPatientDrawer();         // Open Patient Channel Menu.
+        patientdrawer.ChannelMenu('Patient Profile');   // Open Patient Profile in the List. 
+        
+        cy.log("Test Equity Score in the profile");
+        cy.get('[style="font-size: 32px;"]').invoke('text').then(text=>{ // Get Acuity Score in the patient Profile 
+          expect(text).to.eq(score);
+        })
+        
+         cy.log('Test Patient Flag header in the profile..')
+         cy.get('.column .patient-header-flag').then(el=>{   // Get the Patient Flags Header in the Patient Profile 
+          expect(el.length).to.equal(patientFlags);
+         });
+
   })
 });
